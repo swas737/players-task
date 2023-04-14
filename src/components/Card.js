bbc127aa22d5bebe5140a583ef34c98a8bb711b1
swastika <sswas737@gmail.com>
2023-04-14 18:08:16 +0530

    date

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import * as images from '../player-images/imports'
+import moment from 'moment-timezone'
 const Card = ({players,key}) => {
   return (
     <div className="card m-2" style={{width:"18rem"}} key={key}>
@@ -12,10 +13,12 @@ const Card = ({players,key}) => {
        <p className="card-text"><b>SkillDesc</b> : {players.SkillDesc}</p>
        <p className="card-text"><b>$</b>{players.Value}</p>
        <p className="card-text"><b>UpComingMatchesList:</b> {players.UpComingMatchesList[0].CCode} vs {players.UpComingMatchesList[0].VsCCode}</p>
-       <p className="card-text"><b>Next Match: </b>{players.UpComingMatchesList[0].MDate}</p>
-     </div>
-   </div>
-  )
-}
-
-export default Card
+       <p className="card-text"><b>Next Match: </b>
+      {moment(players.UpComingMatchesList[0].MDate).format()}</p>
+       </div>
+       </div>
+       )
+      }
+      
+      // {players.UpComingMatchesList[0].MDate}
+      export default Card
